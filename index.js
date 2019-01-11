347665be1cfbd7a6f53d244574033d33168116ee
Benji Kay <okaybenji@gmail.com>
2019-01-11 15:44:45 -0600

    log midi messages

---
+++ index.js
@@ -0,0 +1,25 @@
+navigator.requestMIDIAccess()
+  .then((midi) => {
+    console.log(`${midi.inputs.size} MIDI devices connected!`);
+
+    const handleMsg = msg => {
+      const [cmd, note, velocity] = msg.data;
+
+      const round = val => val.toFixed(2);
+      const frequency = note => round(Math.pow(2, (note - 69) / 12) * 440) + 'hz';
+      const loudness = velocity => round(velocity / 127) * 100 + '%';
+      const cmds = {
+       '128': 'OFF',
+       '144': 'ON',
+      };
+      const command = cmds[cmd] === 'ON' && velocity === 0 ? 'OFF' : cmds[cmd];
+
+      console.log(command, frequency(note), loudness(velocity));
+    };
+
+    for (const input of midi.inputs.values()) {
+      input.onmidimessage = handleMsg;
+    }
+  }, () => {
+    console.log('Failed to access MIDI');
+  });
