c271f7255f2c5279ccf9aaa8f3423a5090440bbf
Benji Kay <okaybenji@gmail.com>
2019-01-11 16:18:53 -0600

    play midi notes w/ submono

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
+const audioCtx = new AudioContext();
+const synth = new Monosynth(audioCtx);
+
+// Click anywhere in the page to enable sound.
+document.onclick = () => {
+  audioCtx.resume();
+  document.onclick = undefined;
+};
+
 navigator.requestMIDIAccess()
   .then((midi) => {
-    const handleMsg = msg => {
+    const handleMsg = (msg) => {
       const [cmd, note, velocity] = msg.data;
 
       const round = val => val.toFixed(2);
-      const frequency = note => round(Math.pow(2, (note - 69) / 12) * 440) + 'hz';
-      const loudness = velocity => round(velocity / 127) * 100 + '%';
+      const frequency = note => Math.pow(2, (note - 69) / 12) * 440;
+      const loudness = velocity => (velocity / 127) * 100;
       const cmds = {
        '128': 'OFF',
        '144': 'ON',
       };
       const command = cmds[cmd] === 'ON' && velocity === 0 ? 'OFF' : cmds[cmd];
 
-      console.log(command, frequency(note), loudness(velocity));
+      console.log(`${command} ${round(frequency(note))}hz ${round(loudness(velocity))}%`);
+
+      // Play the notes!
+      if (command === 'ON') {
+        synth.pitch(frequency(note));
+        synth.start();
+      } else {
+        synth.stop();
+      }
     };
 
     for (const input of midi.inputs.values()) {
